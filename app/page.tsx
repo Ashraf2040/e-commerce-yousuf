cdf1462f94969337a9d3319df7bdec9e799426f4
Ashraf2040 <m_a199025@yahoo.com>
2025-12-29 21:51:52 +0300

    fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
-
-
 import { Header } from './components/Header';
 import { Carousel } from './components/Carousel';
 import { ProductCard } from './components/ProductCard';
 import { Footer } from './components/Footer';
 import { Sidebar } from './components/Sidebar';
-import {prisma} from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
+import { Prisma } from '@prisma/client';
 
-  const products = await prisma.product.findMany({
+type Product = Prisma.ProductGetPayload<{
+  select: {
+    id: true;
+    slug: true;
+    name: true;
+    category: true;
+    price: true;
+    rating: true;
+    imageUrl: true;
+  };
+}>;
+
+export default async function HomePage() {
+  const products: Product[] = await prisma.product.findMany({
     select: {
       id: true,
       slug: true,
@@ -15,29 +27,30 @@ import {prisma} from '@/lib/prisma';
       category: true,
       price: true,
       rating: true,
-      imageUrl: true
-    }
-  })
-export default function HomePage() {
+      imageUrl: true,
+    },
+  });
+
   return (
     <>
       <Header />
+
       <main className="min-h-screen bg-gray-100">
         <section className="mt-6">
           <Carousel />
         </section>
 
-        <section className=" mx-auto mt-12 px-4 sm:px-6 lg:px-8 flex justify-center items-start gap-4 ">
+        <section className="mx-auto mt-12 px-4 sm:px-6 lg:px-8 flex justify-center items-start gap-4">
           <Sidebar />
+
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {products.map((p, index) => (
-              <ProductCard key={p.name + index} {...p} />
+              <ProductCard key={p.id} {...p} />
             ))}
           </div>
         </section>
-
-       
       </main>
+
       <Footer />
     </>
   );
